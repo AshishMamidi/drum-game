072e598ce0a01e03c44c4a0bb8dcaeb3a524af86
AshishMamidi <88136427+AshishMamidi@users.noreply.github.com>
2022-01-12 12:08:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+// for detecting clicks
+var i = 0;
+while(i < 7)
+{
+document.querySelectorAll(".button")[i].addEventListener("click",function ()
+{
+  var innerhtml = this.innerHTML;
+  makesound(innerhtml);
+})
+i++;
+}
+const tl = gsap.timeline({defaults:{duration:0.5}})
+tl.fromTo(".header-1",{opacity:0},{opacity:1,duration:1})
+tl.fromTo(".button-1",{y:200,opacity:0},{y:0,opacity:1,duration:1})
+tl.fromTo(".button-2",{y:-200,opacity:0},{y:0,opacity:1,duration:1},"<")
+
+// var i = 0;
+// while(i < 7)
+// {
+// document.querySelectorAll("button")[i].addEventListener("click",function ()
+//  {
+//    tl.fromTo("button",{y:20},{y:0,duration:1,ease:"back.out(1.4)"})
+// })
+// i++;
+// }
+//for detecting keyboard presses
+document.addEventListener("keypress",function(event)
+{
+  makesound(event.key);
+})
+
+
+function makesound(keyvalue)
+{
+  switch(keyvalue) {
+    case "w":
+      var sound1 = new Audio("sounds/crash.mp3");
+      sound1.play();
+      break;
+    case "a":
+       var sound2 = new Audio("sounds/kick-bass.mp3");
+       sound2.play();
+       break;
+    case "s":
+       var sound3 = new Audio("sounds/snare.mp3");
+       sound3.play();
+       break;
+    case "d":
+       var sound4 = new Audio("sounds/tom-1.mp3");
+       sound4.play();
+       break;
+    case "f":
+       var sound5 = new Audio("sounds/tom-2.mp3");
+       sound5.play();
+       break;
+    case "g":
+       var sound6 = new Audio("sounds/tom-3.mp3");
+       sound6.play();
+       break;
+    case "h":
+       var sound7 = new Audio("sounds/tom-4.mp3");
+       sound7.play();
+       break;
+  }
+}
